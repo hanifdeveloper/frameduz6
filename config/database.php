e548b9c43adfa9316143cc5606ff2508199b9053
hanifdeveloper <hanif.softdev@gmail.com>
2020-01-16 20:45:07 +0700

    update hanif

--- config/database.php
+++ config/database.php
@@ -15,13 +15,27 @@ return array(
         'host' => 'localhost',
         'port' => 3306,
         'user' => 'root',
-        'password' => '',
+        'password' => 'root',
         'dbname' => 'dbweb_sample',
         'charset' => 'utf8',
         'collate' => 'utf8_general_ci',
         'persistent' => false,
         'errorMsg' => 'Maaf, Gagal terhubung dengan database',
     ),
+    // 'crud' => array(
+    //     'driver' => 'mysql',
+    //     'host' => '127.0.0.1',
+    //     'port' => 13306,
+    //     'user' => 'root',
+    //     'password' => '$pegawai-db@simapp',
+    //     'dbname' => 'db_pegawai',
+    //     'charset' => 'utf8',
+    //     'collate' => 'utf8_general_ci',
+    //     'persistent' => false,
+    //     'errorMsg' => 'Maaf, Gagal terhubung dengan database',
+    //     // Tunneling
+    //     // ssh -N -L 13306:127.0.0.1:3306 root@192.168.254.124
+    // ),
 );
 /*----------------------*/
 ?>
